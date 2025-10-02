82c35a1e940fb18fa574e68348805cad111fb650
Brandon0304 <bortiz_194@cue.edu.co>
2025-10-02 15:18:33 -0500

    Cambios locales en gestor_tareas.py

--- src/gestor_tareas.py
+++ src/gestor_tareas.py
@@ -1,1 +1,18 @@
 print('Bienvenido al Gestor de Tareas en Python')
+
+# Listar tareas - Paradigma POO
+
+class GestorTareas:
+    def __init__(self, tareas):
+        self.tareas = tareas
+
+    def listar_tareas(self):
+        """
+        Muestra las tareas almacenadas.
+        """
+        if not self.tareas:
+            print("No hay tareas.")
+        else:
+            print("📋 Lista de tareas:")
+            for i, tarea in enumerate(self.tareas, 1):
+                print(f"{i}. {tarea}")
