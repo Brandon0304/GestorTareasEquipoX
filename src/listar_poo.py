e5ba877d566675eeb6e244b6febde9eea9e55464
Brandon0304 <bortiz_194@cue.edu.co>
2025-10-02 14:42:58 -0500

    Implementacion de listar tareas en POO

---
+++ src/listar_poo.py
@@ -0,0 +1,16 @@
+# Listar tareas - Paradigma POO
+
+class GestorTareas:
+    def __init__(self, tareas):
+        self.tareas = tareas
+
+    def listar_tareas(self):
+        """
+        Muestra las tareas almacenadas.
+        """
+        if not self.tareas:
+            print("No hay tareas.")
+        else:
+            print("📋 Lista de tareas:")
+            for i, tarea in enumerate(self.tareas, 1):
+                print(f"{i}. {tarea}")
